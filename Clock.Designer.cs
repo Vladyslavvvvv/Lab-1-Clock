49e1f0c320aa6b3e4b9e449bf368a98e0895751d
Vlad <matviykiv.v@nltu.lviv.ua>
2024-02-23 10:10:11 +0200

    Add project files.

---
+++ Clock.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Lab_1_Clock
+{
+    partial class Clock
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.labelHour = new System.Windows.Forms.Label();
+            this.labelMinute = new System.Windows.Forms.Label();
+            this.labelSecond = new System.Windows.Forms.Label();
+            this.buttonSetTime = new System.Windows.Forms.Button();
+            this.textBoxHour = new System.Windows.Forms.TextBox();
+            this.textBoxMinute = new System.Windows.Forms.TextBox();
+            this.textBoxSecond = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // labelHour
+            // 
+            this.labelHour.AutoSize = true;
+            this.labelHour.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelHour.Location = new System.Drawing.Point(320, 88);
+            this.labelHour.Name = "labelHour";
+            this.labelHour.Size = new System.Drawing.Size(42, 38);
+            this.labelHour.TabIndex = 0;
+            this.labelHour.Text = "H";
+            // 
+            // labelMinute
+            // 
+            this.labelMinute.AutoSize = true;
+            this.labelMinute.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelMinute.Location = new System.Drawing.Point(369, 88);
+            this.labelMinute.Name = "labelMinute";
+            this.labelMinute.Size = new System.Drawing.Size(45, 38);
+            this.labelMinute.TabIndex = 1;
+            this.labelMinute.Text = "M";
+            // 
+            // labelSecond
+            // 
+            this.labelSecond.AutoSize = true;
+            this.labelSecond.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelSecond.Location = new System.Drawing.Point(421, 88);
+            this.labelSecond.Name = "labelSecond";
+            this.labelSecond.Size = new System.Drawing.Size(40, 38);
+            this.labelSecond.TabIndex = 2;
+            this.labelSecond.Text = "S";
+            // 
+            // buttonSetTime
+            // 
+            this.buttonSetTime.AutoSize = true;
+            this.buttonSetTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonSetTime.Location = new System.Drawing.Point(74, 300);
+            this.buttonSetTime.Name = "buttonSetTime";
+            this.buttonSetTime.Size = new System.Drawing.Size(119, 39);
+            this.buttonSetTime.TabIndex = 3;
+            this.buttonSetTime.Text = "Set time";
+            this.buttonSetTime.UseVisualStyleBackColor = true;
+            this.buttonSetTime.Click += new System.EventHandler(this.buttonSetTime_Click);
+            // 
+            // textBoxHour
+            // 
+            this.textBoxHour.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxHour.Location = new System.Drawing.Point(225, 302);
+            this.textBoxHour.Name = "textBoxHour";
+            this.textBoxHour.Size = new System.Drawing.Size(100, 34);
+            this.textBoxHour.TabIndex = 4;
+            // 
+            // textBoxMinute
+            // 
+            this.textBoxMinute.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxMinute.Location = new System.Drawing.Point(331, 302);
+            this.textBoxMinute.Name = "textBoxMinute";
+            this.textBoxMinute.Size = new System.Drawing.Size(100, 34);
+            this.textBoxMinute.TabIndex = 5;
+            // 
+            // textBoxSecond
+            // 
+            this.textBoxSecond.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxSecond.Location = new System.Drawing.Point(437, 302);
+            this.textBoxSecond.Name = "textBoxSecond";
+            this.textBoxSecond.Size = new System.Drawing.Size(100, 34);
+            this.textBoxSecond.TabIndex = 6;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            // 
+            // Clock
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxSecond);
+            this.Controls.Add(this.textBoxMinute);
+            this.Controls.Add(this.textBoxHour);
+            this.Controls.Add(this.buttonSetTime);
+            this.Controls.Add(this.labelSecond);
+            this.Controls.Add(this.labelMinute);
+            this.Controls.Add(this.labelHour);
+            this.Name = "Clock";
+            this.Text = "Clock";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelHour;
+        private System.Windows.Forms.Label labelMinute;
+        private System.Windows.Forms.Label labelSecond;
+        private System.Windows.Forms.Button buttonSetTime;
+        private System.Windows.Forms.TextBox textBoxHour;
+        private System.Windows.Forms.TextBox textBoxMinute;
+        private System.Windows.Forms.TextBox textBoxSecond;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Clock.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Lab_1_Clock
+{
+    public partial class Clock : Form
+    {
+        private Time currentTime;
+        private Timer timer;
+
+        public Clock()
+        {
+            InitializeComponent();
+
+            currentTime = new Time();
+            UpdateTimeLabels();
+
+            timer = new Timer();
+            timer.Interval = 1000;
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            currentTime.AddSeconds(1);
+            UpdateTimeLabels();
+        }
+
+        private void UpdateTimeLabels()
+        {
+            labelHour.Text = $"{currentTime.Hours:D2}";
+            labelMinute.Text = $"{currentTime.Minutes:D2}";
+            labelSecond.Text = $"{currentTime.Seconds:D2}";
+        }
+
+        private void buttonSetTime_Click(object sender, EventArgs e)
+        {
+            currentTime.SetTime(int.Parse(textBoxHour.Text), int.Parse(textBoxMinute.Text), int.Parse(textBoxSecond.Text));
+            UpdateTimeLabels();
+        }
+    }
+
+    class Time
+    {
+        private int hours;
+        private int minutes;
+        private int seconds;
+
+        public int Hours
+        {
+            get { return hours; }
+            set
+            {
+                if (value >= 0 && value < 24)
+                    hours = value;
+                else
+                    MessageBox.Show("Invalid hour value");
+            }
+        }
+
+        public int Minutes
+        {
+            get { return minutes; }
+            set
+            {
+                if (value >= 0 && value < 60)
+                    minutes = value;
+                else
+                    MessageBox.Show("Invalid minute value");
+            }
+        }
+
+        public int Seconds
+        {
+            get { return seconds; }
+            set
+            {
+                if (value >= 0 && value < 60)
+                    seconds = value;
+                else
+                    MessageBox.Show("Invalid second value");
+            }
+        }
+
+        public void SetTime(int hours, int minutes, int seconds)
+        {
+            Hours = hours;
+            Minutes = minutes;
+            Seconds = seconds;
+        }
+
+        public void AddSeconds(int seconds)
+        {
+            Seconds += seconds;
+        }
+    }
+}
